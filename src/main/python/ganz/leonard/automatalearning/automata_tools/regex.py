de088252f71762169acbcdc275209f949ebdd966
Leonard Ganz <leonard@ruediger-ganz.de>
2021-12-12 16:43:04 +0100

    Fix regex not matching displayed dfa

--- src/main/python/ganz/leonard/automatalearning/automata_tools/regex.py
+++ src/main/python/ganz/leonard/automatalearning/automata_tools/regex.py
@@ -7,9 +7,8 @@ if not len(sys.argv) == 6:
     raise Exception("Parameters have wrong format!")
 
 dfa = parse_dfa(sys.argv[1:])
-mini_dfa = dfa.reduce()
-print(mini_dfa.__repr__())
-rx = lego.from_fsm(mini_dfa).reduce()
+print(dfa.__repr__())
+rx = lego.from_fsm(dfa).reduce()
 
 print("regex: " + str(rx))
 print("script successful")
---
+++ src/test/java/ganz/leonard/automatalearning/unittests/ScoreTest.java
@@ -0,0 +1,79 @@
+package ganz.leonard.automatalearning.unittests;
+
+import ganz.leonard.automatalearning.automata.general.DeterministicFiniteAutomaton;
+import ganz.leonard.automatalearning.automata.general.DeterministicState;
+import ganz.leonard.automatalearning.gui.optionsscreen.InputProvider;
+import ganz.leonard.automatalearning.learning.AutomataLearning;
+import ganz.leonard.automatalearning.learning.AutomataLearningOptionsBuilder;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Paths;
+import java.sql.SQLOutput;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class ScoreTest {
+
+  private static final String INPUT_FILE =
+      "ganz/leonard/automatalearning/input/AstarB_ScoreTest.txt";
+  private static DeterministicFiniteAutomaton<Object> dfa;
+
+  @BeforeAll
+  static void setUp() {
+    buildInefficientDfa();
+  }
+
+  private static void buildInefficientDfa() {
+    // model a*b inefficiently
+    DeterministicState<Object> fst = new DeterministicState<>(1, false);
+    DeterministicState<Object> snd = new DeterministicState<>(2, true);
+    DeterministicState<Object> as = new DeterministicState<>(3, false);
+    fst.addTransitions(Map.of('a', as, 'b', snd));
+    as.addTransitions(Map.of('a', as, 'b', snd));
+    dfa = new DeterministicFiniteAutomaton<>(Set.of(fst, snd, as), fst);
+  }
+
+  @Test
+  void testAutomatonAcceptance() throws URISyntaxException, IOException {
+    Map<List<Object>, Boolean> input = getInput();
+    input.forEach(
+        (word, inLang) -> {
+//          System.out.println(word + " is inLang: " + inLang + " automaton accepts: " + dfa.accepts(word));
+          Assertions.assertEquals(inLang, dfa.accepts(word));
+        });
+  }
+
+  @Test
+  void testFullScore() throws URISyntaxException, IOException {
+    Map<List<Object>, Boolean> input = getInput();
+    double expectedScore = 1;
+
+    testScore(input, expectedScore);
+  }
+
+  @Test
+  void testPartialScore() throws URISyntaxException, IOException {
+    Map<List<Object>, Boolean> input = getInput();
+    long nrCorrect = input.size();
+    input.put(List.of('a', 'a'), true);
+    double expectedScore = (double) nrCorrect / input.size();
+
+    testScore(input, expectedScore);
+  }
+
+  void testScore(Map<List<Object>, Boolean> input, double expectedScore) {
+    AutomataLearning<Object> al =
+        new AutomataLearning<>(AutomataLearningOptionsBuilder.builder().build(), input);
+
+    double actualScore = al.calcMatchingInputScore(dfa);
+    Assertions.assertEquals(expectedScore, actualScore);
+  }
+
+  private Map<List<Object>, Boolean> getInput() throws IOException, URISyntaxException {
+    return InputProvider.readFromFile(Paths.get(ClassLoader.getSystemResource(INPUT_FILE).toURI()));
+  }
+}
