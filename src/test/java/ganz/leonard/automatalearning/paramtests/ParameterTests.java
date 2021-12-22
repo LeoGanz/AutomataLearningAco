0a827297e313ab09c901a8392c8d59d313322d68
Leonard Ganz <leonard@ruediger-ganz.de>
2021-12-22 23:47:35 +0100

    Update param range for min remaining prob

--- src/test/java/ganz/leonard/automatalearning/paramtests/ParameterTests.java
+++ src/test/java/ganz/leonard/automatalearning/paramtests/ParameterTests.java
@@ -164,13 +164,16 @@ public class ParameterTests {
   @Test
   void testMinRemainingProbability() throws IOException {
     TestDataSaver dataSaver = new TestDataSaver("MinRemainingProbability");
-    for (double minRemainingProb = 0.0001; minRemainingProb < 0.10000001; minRemainingProb *= 2) {
+    for (double minRemainingProb = 0;
+        minRemainingProb < 0.10000001;
+        minRemainingProb += minRemainingProb < .05 ? .002 : .01) {
       PheromoneFunction pheromoneFunction =
           new PheromoneFunction(getDefaultSigmoid(), .1, minRemainingProb);
       AutomataLearningOptions options =
           getDefaultOptionsBuilder().pheromoneFunction(pheromoneFunction).build();
       TestDataSaver.DataSaverSubtest dataSaverSubtest =
-          dataSaver.beginSubtest(options, input, Map.of("minRemainingProb", Util.formatDouble(minRemainingProb, 2)));
+          dataSaver.beginSubtest(
+              options, input, Map.of("minRemainingProb", Util.formatDouble(minRemainingProb, 4)));
       TestRunner.test(options, input, dataSaverSubtest, NR_COLONIES_MEDIUM, false).join();
     }
     dataSaver.close();
